aaa6563f9cc4e7b4abcc26aa6528b106c5c314ce
TheMoss <39796757+TheMoss@users.noreply.github.com>
2020-07-06 22:07:54 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+ //display brew time and temp below
+function randomColor() {
+    let teaColor = ["Black Tea", "Green Tea", "White Tea", "Yellow Tea", "Red Tea"];
+    return teaColor[Math.floor(Math.random() * teaColor.length)];
+}
+
+function randomAdditive() {
+    let teaAdditives = ["Cinnamon", "Pepper", "Raspberry", "Strawberry", "Ginger", "Apple", "Orange Zest", "Lemon Zest", "Peppermint","Clove", "Cardamom", "Cherry", "Red Currant", "Mango", "Rosehip"];
+    return teaAdditives[Math.floor(Math.random() * teaAdditives.length)];
+    }
+document.getElementById("button").onclick = function() {randomTea()};
+
+function randomTea() {
+   document.getElementById("result").innerHTML = randomColor() + ", " + randomAdditive() + ", " + randomAdditive();
+}
